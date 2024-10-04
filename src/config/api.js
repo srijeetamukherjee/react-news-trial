65f14b64ab78734247449bf0175dfbb77de0019f
SRIJEETA MUKHERJEE <srijeeta.mukherjee2021@gmail.com>
2024-10-04 14:56:58 +0530

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const API_DOMAIN = "https://newsapi.org/v2/top-headlines?country=";
 const API_SEARCH_DOMAIN = "https://newsapi.org/v2/everything?q=";
-const API_KEY = "b3e4818de8544a0f943afd7ab61e2eb4"; // Replace with your actual NewsAPI key
+const API_KEY = "My APi Key"; 
 export const endpointPath = (country, category) =>
   `${API_DOMAIN}${country}&category=${category}&apiKey=${API_KEY}`;
 export const endpointSearch = (searchQuery) =>
